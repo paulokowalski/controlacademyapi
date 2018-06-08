0d65c763c94367f2d797d3a0d9ae5ac454e0bdb6
Paulo <paulokowalski@gmail.com>
2018-06-07 22:52:24 -0300

    testando config banco heroku #3

--- src/main/java/com/kowalski/controlacademyapi/cors/CorsFilter.java
+++ src/main/java/com/kowalski/controlacademyapi/cors/CorsFilter.java
@@ -18,20 +18,24 @@ import org.springframework.core.annotation.Order;
 @Configuration
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
+
+	private String origemPermitida = "http://localhost:4200";
 	
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;
-		
-		response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+
+		response.setHeader("Access-Control-Allow-Origin", origemPermitida);
 		response.setHeader("Access-Control-Allow-Credentials", "true");
-		
-		if("OPTIONS".equals(request.getMethod())) {
+
+		if("OPTIONS".equals(request.getMethod()) && origemPermitida.equals(request.getHeader("Origin"))) {
 			response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
 			response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
 			response.setHeader("Access-Control-Allow-Max-Age", "3600");
+
+			response.setStatus(HttpServletResponse.SC_OK);
 		} else {
 			chain.doFilter(req, res);
 		}
